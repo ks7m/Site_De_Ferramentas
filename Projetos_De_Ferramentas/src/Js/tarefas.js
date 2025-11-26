d6086931df177a836056733ff9d3775db80842a0
Kauan Souza <kauazinhoeu62@gmail.com>
2025-11-26 15:55:17 -0300

    Lista de tarefas funcional bassta fazer o css

--- Projetos_De_Ferramentas/src/Js/tarefas.js
+++ Projetos_De_Ferramentas/src/Js/tarefas.js
@@ -7,18 +7,43 @@ const addBtn = document.getElementById("addTaskBtn");
 const list = document.getElementById("taskList");
 
 
-
+addBtn.addEventListener ("click" , addTask);
 // 1. Adicionar tarefa
 
 function addTask() {
     const text = input.value.trim();
 
    
-    if (text === "") return;
+    if (text === "") return; //Evitar a pessoa nao digitar nada e ficar uma tarefa vazia
 
-   
-} // ⏳ Em desenvolvimento. O dev do futuro que lute pra terminar isso.
-  // 🤯 Código em progresso — não pergunte quando vai ficar pronto.
+   // Aqui agora sera o visor -- Kauan Souza
+    const li = document.createElement ("li")
+       li.textContent = text;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remover";
+    removeBtn.classList.add("remove-btn");
+
+    removeBtn.addEventListener("click", (event) => {
+        event.stopPropagation();
+        list.removeChild(li);
+    });
+
+    li.addEventListener("click", () => {
+        li.classList.toggle("completed");
+    });
+
+    li.appendChild(removeBtn);
+    list.appendChild(li);
+    input.value = "";
+}
+
+
+
+
+                            //Isso abaixo ficara nos bastidores
+         // ⏳ Em desenvolvimento. O dev do futuro que lute pra terminar isso.
+         // 🤯 Código em progresso — não pergunte quando vai ficar pronto.
 
 
 
