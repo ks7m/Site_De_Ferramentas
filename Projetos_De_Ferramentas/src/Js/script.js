89dafcb710fdeb05806234fe134c304b7e004e53
Kauan Souza <kauazinhoeu62@gmail.com>
2025-12-11 19:57:13 -0300

    Alteraçoes do js

--- Projetos_De_Ferramentas/src/Js/script.js
+++ Projetos_De_Ferramentas/src/Js/script.js
@@ -1,15 +1,1 @@
-const toggleBtn = document.getElementById("themeToggle");
 
-// Carregar o tema salvo
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark") {
-    document.documentElement.classList.add("dark-theme");
-}
-
-// Alternar o tema 
-toggleBtn.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark-theme");
-
-    const isDark = document.documentElement.classList.contains("dark-theme");
-    localStorage.setItem("theme", isDark ? "dark" : "light");
-});
